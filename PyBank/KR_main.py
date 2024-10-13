284d0a0b509569da57bfa4f0ea3ea86d8ba36969
katmr27 <katrod4336@gmail.com>
2024-10-13 16:31:02 -0700

    resubmit update

--- PyBank/KR_main.py
+++ PyBank/KR_main.py
@@ -4,8 +4,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join(r"C:\Users\katro\python-challenge\PyBank\Resources", "budget_data.csv")# Input file path
-file_to_output = os.path.join(r"C:\Users\katro\python-challenge\PyBank\analysis", "budget_.txt")# Output file path
+file_to_load = os.path.join("Resources", "budget_data.csv")# Input file path
+file_to_output = os.path.join("analysis", "budget_.txt")# Output file path
 
 # Define variables to track the financial data
 total_months = 0
--- PyPoll/KR_main_PyPoll.py
+++ PyPoll/KR_main_PyPoll.py
@@ -4,8 +4,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join(r"C:\Users\katro\python-challenge\PyPoll\Resources", "election_data.csv")  # Input file path
-file_to_output = os.path.join(r"C:\Users\katro\python-challenge\PyPoll\analysis", "election_analysis.txt")  # Output file path
+file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
